0cee3b0271e2788676d719115b9979bbc1e30c99
이우진 <woojinlee@iujin-ui-MacBookPro.local>
2025-04-21 21:05:11 +0900

    embedding 쿼리 테스트 스크립트 추가

---
+++ scripts/embedding_query_test.py
@@ -0,0 +1,41 @@
+import psycopg2
+import numpy as np
+
+from google import genai
+
+from config import GEMINI_API_KEY, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+postgres_connection = psycopg2.connect(
+    dbname=POSTGRES_DB,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASSWORD,
+    host=POSTGRES_HOST
+)
+cursor = postgres_connection.cursor()
+
+
+def get_embedding(content: str, model: str = "gemini-embedding-exp-03-07") -> list[float]:
+    """
+    벡터 테이블 생성 쿼리
+    """
+    result = client.models.embed_content(
+        model=model,
+        contents=content,
+    )
+    return result.embeddings[0].values
+
+
+query = "암을 정의하는 조항에 대해서 설명해줘"
+embedding = get_embedding(query)
+
+print("query", query)
+print("embedding", embedding)
+
+# embedding 리스트를 PostgreSQL vector 타입으로 명시적 캐스팅
+cursor.execute("SELECT * FROM embedding_article ORDER BY embedding <-> %s::vector LIMIT 5", (embedding,))
+
+print("id", "\t", "insurance_name", "\t", "article_title", "\t", "article_content")
+for [id, insurance_name, article_title, article_content, embedding] in cursor.fetchall():
+    print(id, "\t", insurance_name, "\t", article_title, "\t", article_content)
