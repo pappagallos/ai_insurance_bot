69976aa09ae20fca3b5da81de9e9493db5decfe5
이우진 <woojinlee@iujin-ui-MacBookPro.local>
2025-05-01 18:37:58 +0900

    주석 수정

--- insurance_chat_backend/app/controllers/controllers.py
+++ insurance_chat_backend/app/controllers/controllers.py
@@ -6,8 +6,8 @@ from flask import Blueprint, current_app, request, make_response, redirect, stre
 bp_controllers = Blueprint('main', __name__)
 
 
-# 판결문 검색 API
-@bp_controllers.route("/chat", methods=['GET'])
+# 챗봇 API
+@bp_controllers.route("/chat", methods=['POST'])
 def chat_response():
     print("테스트")
     return make_response(json.dumps({"result": True, "data": "테스트"}, ensure_ascii=False), 200)
