341ca44843f9ae4b41b57300fa205c2c26a64be1
이우진 <woojinlee@iujin-ui-MacBookPro.local>
2025-05-17 14:44:02 +0900

    run_extract_testset.py 수정

--- scripts/evaluation/run_extract_testset.py
+++ scripts/evaluation/run_extract_testset.py
@@ -1,13 +1,18 @@
 import os
 import sys
 import json
+import uuid
+import time
 import cohere
 import psycopg2
+import pandas as pd
 from openai import OpenAI
 from google import genai
-import pandas as pd
+from requests.exceptions import HTTPError
+
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 from evaluation.queries import queries
 from config import GEMINI_API_KEY, OPENAI_API_KEY, COHERE_API_KEY, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST
@@ -60,42 +65,52 @@ def get_cosine_result(cursor: psycopg2.extensions.cursor, query: str, top_n: int
         } for [company_name, category, insurance_name, insurance_type, sales_date, index_title, file_path, chapter_title, article_title, article_content, page_number] in cursor.fetchall()]
 
 
-def get_rerank_result(query: str, documents: list[str], top_n: int = 20) -> list[str]:
+
+def get_rerank_result(query: str, documents: list[str], top_n: int = 20, retries: int = 0, max_retries: int = 5) -> list[str]:
     """
     리랭크 결과 반환
     """
-    result = cohere_client.rerank(
-        model="rerank-v3.5",
-        query=query,
-        documents=documents,
-        top_n=top_n,
-    )
-    
-    return [documents[result.results[i].index] for i in range(top_n)]
+    while retries < max_retries:
+        try:
+            result = cohere_client.rerank(
+                model="rerank-v3.5",
+                query=query,
+                documents=documents,
+                top_n=top_n,
+            )
+            return [json.loads(documents[result.results[i].index]) for i in range(top_n)]
+        except cohere.errors.TooManyRequestsError:
+            print("429 오류, 60초 후 다시 실행합니다.")
+            time.sleep(60)
+            retries += 1
+            continue
+        except Exception as error:
+            print("오류가 발생했습니다. 다시 시도합니다.", error)
+            retries += 1
+            continue
+
+
+def get_safe_data(data: any) -> str:
+    return json.dumps(data, ensure_ascii=False).replace("\\", "")
 
 
 data = []
 rerank_data = []
 for query in queries:
-    print("Processing query: ", query)
+    print("Processing: ", query)
     
-    #==================== 자신의 Retriever를 사용해주세요. ===================
+    #==================== 자신의 Retriever를 사용해주세요. ====================
     documents = get_cosine_result(cursor, query)
     #====================================================================
+
     for document in documents:
-        data.append({"query": query, "document": document})
+        document["document_id"] = str(uuid.uuid4())
 
-    documents = [json.dumps(document, ensure_ascii=False) for document in documents]
-    rerank_documents = get_rerank_result(query, documents)
-    for rerank_document in rerank_documents:
-        rerank_data.append({"query": query, "document": rerank_document})
+    for rank, document in enumerate(documents):
+        data.append({"query": query, "document": document, "rank": rank, "relevance_score": None, "relevance_reason": None})
 
-
-cosine_df = pd.DataFrame(columns=["query", "document"], data=data)
-rerank_df = pd.DataFrame(columns=["query", "document"], data=rerank_data)
-
-cosine_df.to_csv("query_document.csv", index=True)
-rerank_df.to_csv("rerank_query_document.csv", index=True)
+cosine_df = pd.DataFrame(columns=["query", "document", "rank", "relevance_score", "relevance_reason"], data=data)
+cosine_df.to_csv("dataset.csv", index=True)
 
 cursor.close()
 conn.close()
